2a767aa40899785bb34cbe09b5eaddb51479562d
Philipp Haidenbauer <haidy777@haidy777.com>
2018-11-21 17:42:57 +0100

    Format & gitignore

--- 2015/day1.test.js
+++ 2015/day1.test.js
@@ -1,51 +1,51 @@
 const {part1, part2} = require('./day1');
 
-describe('Day 1 - Part 1', ()=>{
-    describe('Example 1', ()=>{
-        it('should return floor 0', ()=>{
-            expect(part1('(())')).toBe(0);
-            expect(part1('()()')).toBe(0);
-        });
-    });
-    
-    describe('Example 2', ()=>{
-        it('should return floor 3', ()=>{
-            expect(part1('(((')).toBe(3);
-            expect(part1('(()(()(')).toBe(3);
-        });
-    });
-    
-    describe('Example 3', ()=>{
-        it('should return floor 3', ()=>{
-            expect(part1('))(((((')).toBe(3);
-        });
-    });
-    
-    describe('Example 4', ()=>{
-        it('should return floor -1', ()=>{
-            expect(part1('())')).toBe(-1);
-            expect(part1('))(')).toBe(-1);
-        });
-    });
-    
-    describe('Example 5', ()=>{
-        it('should return floor -3', ()=>{
-            expect(part1(')))')).toBe(-3);
-            expect(part1(')())())')).toBe(-3);
-        });
-    });
+describe('Day 1 - Part 1', () => {
+	describe('Example 1', () => {
+		it('should return floor 0', () => {
+			expect(part1('(())')).toBe(0);
+			expect(part1('()()')).toBe(0);
+		});
+	});
+
+	describe('Example 2', () => {
+		it('should return floor 3', () => {
+			expect(part1('(((')).toBe(3);
+			expect(part1('(()(()(')).toBe(3);
+		});
+	});
+
+	describe('Example 3', () => {
+		it('should return floor 3', () => {
+			expect(part1('))(((((')).toBe(3);
+		});
+	});
+
+	describe('Example 4', () => {
+		it('should return floor -1', () => {
+			expect(part1('())')).toBe(-1);
+			expect(part1('))(')).toBe(-1);
+		});
+	});
+
+	describe('Example 5', () => {
+		it('should return floor -3', () => {
+			expect(part1(')))')).toBe(-3);
+			expect(part1(')())())')).toBe(-3);
+		});
+	});
 });
 
-describe('Day 1 - Part 2', ()=>{
-    describe('Example 1', ()=>{
-        it('should return when floor -1 is entered', ()=>{
-            expect(part2(')')).toBe(1);
-        });
-    });
-    
-    describe('Example 2', ()=>{
-        it('should return when floor -1 is entered', ()=>{
-            expect(part2('()())')).toBe(5);
-        });
-    });
+describe('Day 1 - Part 2', () => {
+	describe('Example 1', () => {
+		it('should return when floor -1 is entered', () => {
+			expect(part2(')')).toBe(1);
+		});
+	});
+
+	describe('Example 2', () => {
+		it('should return when floor -1 is entered', () => {
+			expect(part2('()())')).toBe(5);
+		});
+	});
 });
--- 2015/day2.js
+++ 2015/day2.js
@@ -1,56 +1,56 @@
-function calcSide (a, b) {
-    return a * b;
+function calcSide(a, b) {
+	return a * b;
 }
 
-module.exports.part1 = function (input){
-    let neededSquareFeet = 0;
-    
-    for(const packDim of input){
-        const packDimSplit = packDim.split('x').map(Number);
-        const [l, w, h] = packDimSplit;
-        let smallestSide = calcSide(l, w);
-        
-        neededSquareFeet += 2 * smallestSide;
-        
-        const sideB = calcSide(w, h);
-        
-        if(sideB < smallestSide){
-            smallestSide = sideB;
-        }
-        
-        neededSquareFeet += 2 * sideB;
-        
-        const sideC = calcSide(h, l);
-        
-        if(sideC < smallestSide){
-            smallestSide = sideC;
-        }
-        
-        neededSquareFeet += 2 * sideC;
-        
-        neededSquareFeet += smallestSide;
-    }
-    
-    return neededSquareFeet;
-}
+module.exports.part1 = function (input) {
+	let neededSquareFeet = 0;
 
-module.exports.part2 = function (input){
-    let neededFeet = 0;
-    
-    for(const packDim of input){
-        const packDimSplit = packDim.split('x').map(Number);
-        const [l, w, h] = packDimSplit;
-        const smallestSide = Math.min(...packDimSplit);
-        
-        packDimSplit.splice(packDimSplit.indexOf(smallestSide), 1);
-        
-        const secondSmallestSide = Math.min(...packDimSplit);
-        
-        neededFeet += (2 * smallestSide) + (2 * secondSmallestSide);
-        
-        neededFeet += l * w * h;
-        
-    }
-    
-    return neededFeet;
-}
+	for (const packDim of input) {
+		const packDimSplit = packDim.split('x').map(Number);
+		const [l, w, h] = packDimSplit;
+		let smallestSide = calcSide(l, w);
+
+		neededSquareFeet += 2 * smallestSide;
+
+		const sideB = calcSide(w, h);
+
+		if (sideB < smallestSide) {
+			smallestSide = sideB;
+		}
+
+		neededSquareFeet += 2 * sideB;
+
+		const sideC = calcSide(h, l);
+
+		if (sideC < smallestSide) {
+			smallestSide = sideC;
+		}
+
+		neededSquareFeet += 2 * sideC;
+
+		neededSquareFeet += smallestSide;
+	}
+
+	return neededSquareFeet;
+};
+
+module.exports.part2 = function (input) {
+	let neededFeet = 0;
+
+	for (const packDim of input) {
+		const packDimSplit = packDim.split('x').map(Number);
+		const [l, w, h] = packDimSplit;
+		const smallestSide = Math.min(...packDimSplit);
+
+		packDimSplit.splice(packDimSplit.indexOf(smallestSide), 1);
+
+		const secondSmallestSide = Math.min(...packDimSplit);
+
+		neededFeet += (2 * smallestSide) + (2 * secondSmallestSide);
+
+		neededFeet += l * w * h;
+
+	}
+
+	return neededFeet;
+};
