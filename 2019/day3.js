ee76ae6e22f30107958e50ee11b4f4c1cc5d04fc
Haidy777 <haidy777@haidy777.com>
2019-12-03 08:21:54 +0100

    2019 Day 2

--- 2019/day3.js
+++ 2019/day3.js
@@ -1,13 +1,6 @@
 function sortByPosition(a, b){
     const [aX, aY] = a.split('|').map(Number);
     const [bX, bY] = b.split('|').map(Number);
-    if (aX == bX) return aY - bY;
-    return aX - bX;
-}
-
-function sortByPosition2(a, b){
-    const [aX, aY] = a.split('|').map(Number);
-    const [bX, bY] = b.split('|').map(Number);
   if (aY == bY) return aX - bX;
   return aY - bY;
 }
@@ -60,17 +53,68 @@ function part1 (wire1, wire2){
         }
     }
 
-    console.log(crossingPoints);
-
-    crossingPoints.sort(sortByPosition2);
+    crossingPoints.sort(sortByPosition);
 
     const mostCentered = crossingPoints[0].split('|');
 
     return Math.abs(Number(mostCentered[0])) + Math.abs(Number(mostCentered[1]));
 }
 
-function part2 (){
+function part2 (wire1, wire2){
+    const posWire1 = new Map();
+    const crossingSteps = [];
+    let x = 0;
+    let y = 0;
+    let steps = 0;
     
+    for(const [direction, ...count] of wire1){
+        const realCount = Number(count.join(''));
+
+        for(let i = 0; i < realCount; i++){
+            steps++;
+            
+            if(direction === 'R'){
+                x++;
+            } else if (direction === 'L'){
+                x--;
+            } else if (direction === 'U'){
+                y++;
+            } else if (direction === 'D'){
+                y--;
+            }
+
+            posWire1.set(`${x}|${y}`, steps);
+        }
+    }
+
+    x = 0;
+    y = 0;
+    steps = 0;
+    for(const [direction, ...count] of wire2){
+        const realCount = Number(count.join(''));
+
+        for(let i = 0; i < realCount; i++){
+            steps++;
+            
+            if(direction === 'R'){
+                x++;
+            } else if (direction === 'L'){
+                x--;
+            } else if (direction === 'U'){
+                y++;
+            } else if (direction === 'D'){
+                y--;
+            }
+
+            const point = `${x}|${y}`;
+
+            if(posWire1.has(point)){
+                crossingSteps.push(posWire1.get(point) + steps);
+            }
+        }
+    }
+
+    return crossingSteps.sort((a,b)=>a-b)[0];
 }
 
 module.exports = {part1, part2};
--- 2019/day3.test.js
+++ 2019/day3.test.js
@@ -13,10 +13,18 @@ describe('Day 3 - Part 1', ()=>{
 		expect(part1(['R98','U47','R26','D63','R33','U87','L62','D20','R33','U53','R51'], ['U98','R91','D20','R16','D67','R40','U7','R15','U6','R7'])).toBe(135);
 	});
 	it('My Input', ()=>{
-		expect(part1(input[0], input[1])).toBe(135);
+		expect(part1(input[0], input[1])).toBe(227);
 	});
 });
 
 describe('Day 3 - Part 2', ()=>{
-
+	it('Example 1', ()=>{
+		expect(part2(['R75','D30','R83','U83','L12','D49','R71','U7','L72'], ['U62','R66','U55','R34','D71','R55','D58','R83'])).toBe(610);
+	});
+	it('Example 2', ()=>{
+		expect(part2(['R98','U47','R26','D63','R33','U87','L62','D20','R33','U53','R51'], ['U98','R91','D20','R16','D67','R40','U7','R15','U6','R7'])).toBe(410);
+	});
+	it('My Input', ()=>{
+		expect(part2(input[0], input[1])).toBe(20286);
+	});
 });
