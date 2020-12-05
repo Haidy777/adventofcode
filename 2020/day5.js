006a85b2d8579863e0c36b3c4965f484d9379944
Haidy777 <haidy777@haidy777.com>
2020-12-05 13:43:27 +0000

    2020 Day 5

--- 2020/day5.js
+++ 2020/day5.js
@@ -1,7 +1,25 @@
 module.exports.part1 = function (input) {
+    input = input.map((i) => {
+        const row = parseInt(i.slice(0, 7).replace(/F/g, '0').replace(/B/g, '1'), 2) * 8;
+        const col = parseInt(i.slice(7).replace(/L/g, '0').replace(/R/g, '1'), 2);
 
+        return row + col;
+    });
+
+    return Math.max(...input);
 };
 
 module.exports.part2 = function (input) {
+    input = input.map((i) => {
+        const row = parseInt(i.slice(0, 7).replace(/F/g, '0').replace(/B/g, '1'), 2) * 8;
+        const col = parseInt(i.slice(7).replace(/L/g, '0').replace(/R/g, '1'), 2);
 
+        return row + col;
+    });
+
+    input = input.sort((a, b) => a - b);
+
+    return input.reduce((last, current, index, allSeats) =>
+        !index || last !== undefined || allSeats[index - 1] + 1 === current ? last : allSeats[index - 1] + 1
+        , undefined);
 };
