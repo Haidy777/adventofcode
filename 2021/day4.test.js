808feaf7eab33b24d8f2c5b6e33c7f6f486f119c
Haidy777 <haidy777@haidy777.com>
2021-12-03 16:46:35 +0000

    2021 Day 4 & 5 prep

---
+++ 2021/day4.test.js
@@ -0,0 +1,26 @@
+const {part1, part2} = require('./day4');
+
+const example1 = [];
+const myInput = [];
+
+describe('Day4', () => {
+  describe('part1', () => {
+    it('Example 1', () => {
+      expect(part1(example1)).toEqual(-1);
+    });
+
+    it('My Input', () => {
+      expect(part1(myInput)).toEqual(-1);
+    });
+  });
+
+  describe('part2', () => {
+    it('Example 1', () => {
+      expect(part2(example1)).toEqual(-1);
+    });
+
+    it('My Input', () => {
+      expect(part2(myInput)).toEqual(-1);
+    });
+  });
+});
