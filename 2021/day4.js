a3db14e29f6adc49ebfe2d3f9df7b7912c1648bc
Haidy777 <haidy777@haidy777.com>
2021-12-04 16:01:29 +0000

    2021 Day 4 Part 1

--- 2021/day4.js
+++ 2021/day4.js
@@ -1,20 +1,72 @@
-function part1 ([drawnNumbers, boards]) {
-    for(const drawnNumber of drawnNumbers){        
-        for(const board of boards){
-            for(const col of board){
-                for(let row of col){                    
-                    if(drawnNumber === row){
-                        row = -1;
+function part1([drawnNumbers, boards]) {
+    console.log(boards);
+    let unmatchedSum = 0;
+    let foundByNumber = 0;
+
+    for (let dn = 0; dn < drawnNumbers.length; dn++) {
+        const drawnNumber = drawnNumbers[dn];
+
+        for (let b = 0; b < boards.length; b++) {
+            const board = boards[b];
+
+            for (let c = 0; c < board.length; c++) {
+                const col = board[c];
+
+                for (let r = 0; r < col.length; r++) {
+                    if (col[r] === drawnNumber) {
+                        boards[b][c][r] = -1;
                     }
                 }
             }
         }
 
-        console.log(boards);
+        if (dn >= 5) {
+            // only after 5 iterations there could be a bingo
+
+            for (const board of boards) {
+                for (const col of board) {
+                    if (col.reduce((p, n) => p + n, 0) === -5) {
+                        // found horizontal match
+                        unmatchedSum = board.flat().filter((e) => e !== -1).reduce((p, n) => p + n, 0);
+                        foundByNumber = drawnNumber;
+                        break;
+                    }
+                }
+
+                if (!unmatchedSum) {
+                    const tempBoard = [[], [], [], [], []];
+
+                    for (let c = 0; c < 5; c++) {
+                        const col = board[c];
+
+                        for (let r = 0; r < 5; r++) {
+                            const row = col[r];
+
+                            tempBoard[r][c] = row;
+                        }
+                    }
+
+                    for (const col of tempBoard) {
+                        if (col.reduce((p, n) => p + n, 0) === -5) {
+                            // found horizontal match
+                            unmatchedSum = board.flat().filter((e) => e !== -1).reduce((p, n) => p + n, 0);
+                            foundByNumber = drawnNumber;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (unmatchedSum && foundByNumber) {
+                break;
+            }
+        }
     }
+
+    return foundByNumber * unmatchedSum;
 }
 
-function part2 ([drawnNumbers, boards]) {
+function part2([drawnNumbers, boards]) {
 }
 
-module.exports = {part1, part2};
+module.exports = { part1, part2 };
