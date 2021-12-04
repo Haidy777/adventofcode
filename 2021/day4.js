d7da1e80b601ec5ebf15029c0c54dd9860a1785b
Haidy777 <haidy777@haidy777.com>
2021-12-04 06:42:51 +0000

    2021 Day 4 WIP

--- 2021/day4.js
+++ 2021/day4.js
@@ -1,7 +1,20 @@
-function part1 (input) {
+function part1 ([drawnNumbers, boards]) {
+    for(const drawnNumber of drawnNumbers){        
+        for(const board of boards){
+            for(const col of board){
+                for(let row of col){                    
+                    if(drawnNumber === row){
+                        row = -1;
+                    }
+                }
+            }
+        }
+
+        console.log(boards);
+    }
 }
 
-function part2 (input) {
+function part2 ([drawnNumbers, boards]) {
 }
 
 module.exports = {part1, part2};
