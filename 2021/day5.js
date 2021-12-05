cc06fe2d9fc4aff7ab65311d586d5c743b1880d2
Haidy777 <haidy777@haidy777.com>
2021-12-05 10:05:11 +0000

    2021 Day 5 Part 1

--- 2021/day5.js
+++ 2021/day5.js
@@ -1,7 +1,52 @@
-function part1 (input) {
+function part1(input) {
+    input = input.split('\n').filter(Boolean).map((i) => i.split('->').map((inp) => inp.trim().split(',').map(Number)));
+    const map = new Map();
+
+    for (let [[startX, startY], [endX, endY]] of input) {
+        if (startX === endX || startY === endY) {
+            if(endX < startX || endY < startY){
+                const tempX = startX;
+                const tempY = startY;
+    
+                startX = endX;
+                startY = endY;
+    
+                endX = tempX;
+                endY = tempY;
+            }
+            
+            if (startX !== endX) {
+                const diff = endX - startX;
+
+                for (let i = 0; i <= diff; i++) {
+                    const coords = `${startX + i}x${startY}`;
+
+                    if (map.has(coords)) {
+                        map.set(coords, map.get(coords) + 1);
+                    } else {
+                        map.set(coords, 1);
+                    }
+                }
+            } else if (startY !== endY) {
+                const diff = endY - startY;
+
+                for (let i = 0; i <= diff; i++) {
+                    const coords = `${startX}x${startY + i}`;
+
+                    if (map.has(coords)) {
+                        map.set(coords, map.get(coords) + 1);
+                    } else {
+                        map.set(coords, 1);
+                    }
+                }
+            }
+        }
+    }
+
+    return Array.from(map.values()).filter((v) => v >= 2).length;
 }
 
-function part2 (input) {
+function part2(input) {
 }
 
-module.exports = {part1, part2};
+module.exports = { part1, part2 };
